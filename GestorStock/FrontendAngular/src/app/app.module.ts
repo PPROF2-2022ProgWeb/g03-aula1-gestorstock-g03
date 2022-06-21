4a78904aef6704b931cdd3c22b71ed445934a943
Osmar Debegnach <andresdebegnach@gmail.com>
2022-06-21 15:49:46 -0300

    Agregue la metadata al sitio

--- GestorStock/FrontendAngular/src/app/app.module.ts
+++ GestorStock/FrontendAngular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { InputComponent } from './components/input/input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
