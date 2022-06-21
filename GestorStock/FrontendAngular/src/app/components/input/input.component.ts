8c34cc9bb01b74bbdae3c2af4391d808d335fd07
Osmar Debegnach <andresdebegnach@gmail.com>
2022-06-21 15:49:03 -0300

    Cree el componente input

---
+++ GestorStock/FrontendAngular/src/app/components/input/input.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent {
+  @Input() titulo: String = 'Titulo';
+  @Input() icono: String = 'icono-user';
+}
