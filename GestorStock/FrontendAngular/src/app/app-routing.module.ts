9f9aad6e17200ea05fb71775e43cd989fcea7ac5
Osmar Debegnach <andresdebegnach@gmail.com>
2022-08-22 13:11:46 -0300

    Relocacion de archivos Cambié la ubicación de los componentes que conforman las páginas. Pasaron de estar en src/app/components a src/app/pages

--- GestorStock/FrontendAngular/src/app/app-routing.module.ts
+++ GestorStock/FrontendAngular/src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { NoEncontradoComponent } from './components/no-encontrado/no-encontrado.component';
-import { QuienesSomosComponent } from './components/quienes-somos/quienes-somos.component';
-import { RegistroComponent } from './components/registro/registro.component';
-import { TiendaComponent } from './components/tienda/tienda.component';
 import { HomeLayoutComponent } from './layouts/home-layout/home-layout.component';
 import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
 import { VentasComponent } from './components/dashboard/ventas/ventas.component';
 import { AdministracionComponent } from './components/dashboard/administracion/administracion.component';
 import { StockComponent } from './components/dashboard/stock/stock.component';
 import { CajaComponent } from './components/dashboard/caja/caja.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { QuienesSomosComponent } from './pages/quienes-somos/quienes-somos.component';
+import { TiendaComponent } from './pages/tienda/tienda.component';
+import { NoEncontradoComponent } from './pages/no-encontrado/no-encontrado.component';
 
 const routes: Routes = [
   {path: '', component: HomeLayoutComponent, children: [
