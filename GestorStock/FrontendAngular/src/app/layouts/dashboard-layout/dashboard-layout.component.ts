21b768492532eca50c853cb6af1e800d30ec408c
Osmar Debegnach <odebegnach@santandertecnologia.com.ar>
2022-09-01 19:07:28 -0300

    TSK#2 Fix scroll de dashboard layout

--- GestorStock/FrontendAngular/src/app/layouts/dashboard-layout/dashboard-layout.component.ts
+++ GestorStock/FrontendAngular/src/app/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,6 +8,11 @@ import { Router } from '@angular/router';
 })
 export class DashboardLayoutComponent implements OnInit {
   public isMenuActive: boolean = false;
+  public scrollActive: boolean = false;
+
+  public scrollTimeout: any;
+
+  @ViewChild('main') main:ElementRef;
 
   constructor(private router: Router) {
     router.events.subscribe(() => {
@@ -15,8 +20,22 @@ export class DashboardLayoutComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 
+  ngAfterViewInit(): void {
+    console.log(this.scrollTimeout);
+    this.main.nativeElement.addEventListener('scroll', () => {
+      this.scrollActive = true;
+      if(this.scrollTimeout){
+        clearTimeout(this.scrollTimeout);
+      }
+      this.scrollTimeout = setTimeout(()=>{
+        this.scrollActive = false;
+      }, 2000)
+    });
+    // this.main.addEventListener('scroll', this.scrollActive)
+  }
   toggleMenu() {
     this.isMenuActive = !this.isMenuActive;
   }
