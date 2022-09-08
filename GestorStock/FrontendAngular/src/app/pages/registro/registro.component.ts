c6dc61178bbd90120f5bc64abacd38ebdf2001ab
Erika <erikac.ec80@gmail.com>
2022-09-08 16:59:59 -0300

    agregue atributos de la clase y validaciones

--- GestorStock/FrontendAngular/src/app/pages/registro/registro.component.ts
+++ GestorStock/FrontendAngular/src/app/pages/registro/registro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RegistroService } from 'src/app/servicios/registro.service';
 import { Iconos } from 'src/app/utils/iconos.enum';
 
 @Component({
@@ -7,19 +10,96 @@ import { Iconos } from 'src/app/utils/iconos.enum';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
-  icon = Iconos;
+
+  nombre = new FormControl('', []);
+  apellido = new FormControl('', []);
+  pais = new FormControl('');
+  provincia = new FormControl('', []);
+  fechaNacimiento = new FormControl('');
+  confirmarContraseña = new FormControl('', [], []);
+  empresa = new FormControl('', []);
+  descripcion = new FormControl('', []);
+  mail = new FormControl('', [], []);
+  password = new FormControl('', [], []);
+  
+  form: any;
+
+
   mostrar = false;
+  public iconos = Iconos;
 
-
-
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+    private registroService: RegistroService,
+    private router: Router) {
+    this.form = this.formBuilder.group({
+      nombre: ['', [Validators.minLength(4)]],
+      apellido: ['', [Validators.minLength(4)]],
+      pais: [Validators.required],
+      provincia: ['', [Validators.minLength(4)]],
+      fechaNacimiento: [Validators.required],
+      confirmarContraseña: ['', [Validators.required, Validators.minLength(6)]],
+      empresa: ['', [Validators.minLength(4)]],
+      descripcion: ['', [Validators.minLength(10)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      mail: ['', [Validators.required, Validators.email]]
+    });
+  }
 
   ngOnInit(): void {
   }
 
-  
-  cargarEmpresa(){
+
+  cargarEmpresa() {
     this.mostrar = true;
+  }
+
+  
+  get mailValid(){
+    return this.form.get('mail')?.touched && !this.form.get('mail')?.valid;
+  }
+
+  get mailErrorMessage(){
+    if(this.mail.hasError('required')){
+      return "El correo es requerido"
+    }else {
+      return "Formato de correo no valido"
+    }
+  }
+
+  get passwordValid(){
+    return this.form.get('password')?.touched && !this.form.get('password')?.valid;
+  }
+
+  get passwordErrorMessage(){
+    return "Debe contener mínimo 6 caracteres"
+  }
+
+  get nombreValid(){
+    return !this.form.get('nombre')?.valid;
+  }
+  get nombreErrorMessage(){
+    return "Debe contener mínimo 4 caracteres"
+  }
+
+  get apellidoValid(){
+    return !this.form.get('apellido')?.valid;
+  }
+  get apellidoErrorMessage(){
+    return "Debe contener mínimo 4 caracteres"
+  }
+
+  
+
+  
+
+  registro(){
+    this.registroService.registro(this.mail, this.password)
+    .then(() => {
+      this.router.navigate(['login']);
+    })
+    .catch(() => {
+      alert("Revise los datos ingresados.")
+    });    
   }
 
 
