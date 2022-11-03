9f32529acf5c729e0e498cc71371f80006177f48
Erika <erikac.ec80@gmail.com>
2022-11-03 17:50:03 -0300

    creando los componentes a utilizar en las paginas de la tienda

---
+++ GestorStock/FrontendAngular/src/app/pages/tienda/tienda-home/tienda-home.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tienda',
+  templateUrl: './tienda-home.component.html',
+  styleUrls: ['./tienda-home.component.css']
+})
+export class TiendaComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+ 
+}
