47cb469252f21011b7e1d1bcd82ff786689716b1
Erika <erikac.ec80@gmail.com>
2022-11-04 14:26:39 -0300

    cree nuevo layout para tienda

---
+++ GestorStock/FrontendAngular/src/app/layouts/tienda-layouts/tienda-layouts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tienda-layouts',
+  templateUrl: './tienda-layouts.component.html',
+  styleUrls: ['./tienda-layouts.component.css']
+})
+export class TiendaLayoutsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
