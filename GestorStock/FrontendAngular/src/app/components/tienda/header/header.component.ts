ae9c5e9c0e42f4b214fba99565560b89d8423c8d
Erika <erikac.ec80@gmail.com>
2022-11-11 10:28:36 -0300

    probando cambios varios

--- GestorStock/FrontendAngular/src/app/components/tienda/header/header.component.ts
+++ GestorStock/FrontendAngular/src/app/components/tienda/header/header.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 import { SearchResult } from 'src/app/interfaces/searchResult';
 import { ProductoModel } from 'src/app/models/ProductoModel';
+import { LoginService } from 'src/app/services/login.service';
 import { ProductosService } from 'src/app/services/productos.service';
+import { RegistroService } from 'src/app/services/registro.service';
 import { Iconos } from 'src/app/utils/iconos.enum';
 
 @Component({
@@ -13,11 +17,19 @@ export class HeaderComponent implements OnInit {
 
   public productos: ProductoModel[];
   public iconos = Iconos;
+  
+  isLoggedIn: boolean;
+  loggedInUser: string;
+  permitirRegistro: boolean;
 
-
-  constructor(private prodServ: ProductosService) { }
+  constructor(private prodServ: ProductosService,
+              private logServ: LoginService,
+              private regServ: RegistroService,
+              private router: Router) { }
 
   ngOnInit(): void {
+   
+
     this.prodServ.getAllProducts().subscribe(data => {
       this.productos = data;
       console.log(this.productos);
@@ -29,9 +41,20 @@ export class HeaderComponent implements OnInit {
   }
 
   loguin(){
+    this.logServ.getAuth().subscribe((auth: { email: string; }) => {
+      if(auth?.email){
+        this.isLoggedIn = true;
+        this.loggedInUser = auth.email;
+        this.permitirRegistro = false;
+      }
+      else{
+        this.isLoggedIn = false;
+        this.permitirRegistro = true;
+      }
+    });
+    
     
   }
   
-
 
 }
