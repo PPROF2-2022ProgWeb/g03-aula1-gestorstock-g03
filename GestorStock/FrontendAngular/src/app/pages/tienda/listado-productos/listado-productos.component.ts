f63290a45af9a85912a693e0929ba51215f62652
Osmar Debegnach <andresdebegnach@gmail.com>
2022-11-16 02:01:05 -0300

    TSK#25 Se implemento la busqueda por nombre de producto

--- GestorStock/FrontendAngular/src/app/pages/tienda/listado-productos/listado-productos.component.ts
+++ GestorStock/FrontendAngular/src/app/pages/tienda/listado-productos/listado-productos.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductoModel } from 'src/app/models/ProductoModel';
+import { ProductosService } from 'src/app/services/productos.service';
 
 @Component({
   selector: 'app-listado-productos',
   templateUrl: './listado-productos.component.html',
-  styleUrls: ['./listado-productos.component.css']
+  styleUrls: ['./listado-productos.component.css'],
 })
 export class ListadoProductosComponent implements OnInit {
-
-  constructor() { }
+  public productos: ProductoModel[] = [];
+  constructor(private route: ActivatedRoute, private ps: ProductosService) {}
 
   ngOnInit(): void {
+    this.route.url.subscribe({
+      next: () => {
+        let param = this.route.snapshot.paramMap.get('busqueda');
+        if (param) {
+          this.ps.cargarPorNombreProducto(param).subscribe({
+            next: (data) => {
+              this.productos = data;
+            },
+            error: (err) => console.log(err),
+          });
+        } else {
+          this.ps.cargarProductos().subscribe({
+            next: (data) => {
+              this.productos = data;
+            },
+            error: (err) => console.log(err),
+          });
+        }
+      },
+    });
   }
-
 }
