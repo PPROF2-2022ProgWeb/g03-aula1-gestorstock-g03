2fadbf6e2075ed0a192b75cda82e9ecf9375c6fc
Erika <erikac.ec80@gmail.com>
2022-11-16 01:55:32 -0300

    probando pasarela de pago

--- GestorStock/FrontendAngular/src/app/pages/tienda/checkout/checkout.component.ts
+++ GestorStock/FrontendAngular/src/app/pages/tienda/checkout/checkout.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
+declare var paypal: any;
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
+  
+  @ViewChild('paypal', { static: true }) paypalElement : ElementRef;
 
-  constructor() { }
+  producto = {
+    descripcion: 'producto en venta',
+    precio : 10.99,
+    img : 'img producto'
+  }
+  title = 'angular-paypal-payment';
 
   ngOnInit(): void {
+    paypal
+    .Buttons({
+      createOrder: (data: any, actions: any) =>{
+        return actions.order.create({
+          purchase_units:[
+            {
+              description: this.producto.descripcion,
+              amount : {
+                currency_code: 'USD',
+                value : this.producto.precio
+              }
+            }
+          ]
+        })
+      },
+      onApprove: async (data: any, actions: any) => {
+        const order = await actions.order.capture();
+        console.log(order);
+      },
+      onError: (err: any) => {
+        console.log(err);
+      }
+    })
+    .render(this.paypalElement.nativeElement);
   }
 
+  
+    
 }
